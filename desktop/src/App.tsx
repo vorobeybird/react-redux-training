f315f27f33f3917bb66c5e0eb9052a3741af6075
IvanStupchuk <hardtrynew@rambler.ru>
2023-03-07 18:10:19 +0100

    app initialization, base setting, eslint

---
+++ desktop/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import './styles/main.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>REACT-REDUX TRAIN PROJECT</h1>
+    </div>
+  )
+}
+
+export default App
