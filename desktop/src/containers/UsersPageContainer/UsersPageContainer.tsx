fdfcdaba3c7bbde4b7a2d6f1628c5ad598ecaa7d
IvanStupchuk <hardtrynew@rambler.ru>
2023-03-13 21:52:13 +0100

    lesson 2: improved request

--- desktop/src/containers/UsersPageContainer/UsersPageContainer.tsx
+++ desktop/src/containers/UsersPageContainer/UsersPageContainer.tsx
@@ -8,9 +8,11 @@ import WrapperHoc from '../../components/WrapperHoc'
 const UsersPageContainer = () => {
   const [users, setUsers] = React.useState<Array<UserType>>([])
 
-  !users.length && fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((json) => setUsers(json))
+  React.useEffect(() => {
+    !users.length && fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((json) => setUsers(json))
+  }, [users])
 
   return (
     <WrapperHoc>
